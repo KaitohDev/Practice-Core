387bebb121111ddd98f9b4671e0ce8cef34be63f
pepa <91153189+pepaoing@users.noreply.github.com>
2022-05-22 22:40:51 +0200

    Command Map

--- src/HyperDevs/Managers/CommandsManager.php
+++ src/HyperDevs/Managers/CommandsManager.php
@@ -1,1 +1,14 @@
+<?php
 
+namespace hyperdevs\staffmode\command;
+
+use hyperdevs\staffmode\Main;
+use hyperdevs\staffmode\command\StaffModeCommand;
+
+class CommandManager {
+    
+    public function __construct(Main $main) {
+        // Registrar commandos
+        $cmdMap = $main->getCommandMap(){
+    }
+}
